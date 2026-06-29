ca5bd903634f33d16dd434863f1ebe428c667255
Rintsen Sherpa <rintsen@umich.edu>
2026-06-29 15:08:57 -0400

    Added config option to run all TLand models and adjusted formatting of score column name

--- workflow/rules/predict.smk
+++ workflow/rules/predict.smk
@@ -24,6 +24,8 @@ rule predict:
         "../envs/TLand.yml"
     resources:
         mem_mb=config['memory_predict_organsp_mb']
+    params:
+        all_TLand_models = "--all_TLand_models" if config["all_TLand_models"] else ""
     shell:
         """
         python workflow/scripts/predict.py \
@@ -33,5 +35,6 @@ rule predict:
            --organ_mapping_json {config[organ_mapping_json]} \
            --organ_list {config[organ_list_path]} \
            --models_path {config[models_path]} \
+           {params.all_TLand_models} \
            --out {output.file} &> {log}
         """
--- workflow/scripts/predict.py
+++ workflow/scripts/predict.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 
 import pandas as pd
 
-def predict(organ, generic_features, organsp_features, gt_100_tf_chip, model_ls):
+def predict(organ, generic_features, organsp_features, selected_model_ls):
     
     pred_dict = defaultdict(defaultdict(list).copy)
     df_all = pd.read_parquet(generic_features)
@@ -19,10 +19,8 @@ def predict(organ, generic_features, organsp_features, gt_100_tf_chip, model_ls)
                 on=['chrom', 'end'], how='left')
 
     # make prediction
-    if organ in gt_100_tf_chip:
-        pred_dict[organ][model_ls[0][0]].append(model_ls[0][1].predict_proba(df_all)[:, 1]) # TLand
-    else:
-        pred_dict[organ][model_ls[1][0]].append(model_ls[1][1].predict_proba(df_all)[:, 1]) # TLand lightest
+    for model_name, model in selected_model_ls:
+        pred_dict[organ][model_name].append(model.predict_proba(df_all)[:, 1])
 
     return df_all, pred_dict
 
@@ -34,19 +32,22 @@ if __name__ == '__main__':
     parser.add_argument('--organ_mapping_json', type=str, required=True, help='Path to file mapping organ names with underscores to organ names with spaces')
     parser.add_argument("--organ_list", help="List of organs TLand can make predictions for", type=str)
     parser.add_argument("--models_path", help="Path to directory containing model files", type=str)
+    parser.add_argument("--all_TLand_models", help="Use all TLand models for prediction", action="store_true")
     parser.add_argument("--out", help="Output file path", type=str)
     args = parser.parse_args()
 
     generic_features = Path(args.generic_features)
     organsp_features = args.organsp_features
+    safe_organ_name = args.organ
     organ_list = args.organ_list
     models_path = Path(args.models_path)
+    all_TLand_models = args.all_TLand_models
     outfile = args.out
 
     # Get organ name with spaces since that is how they are labeled in RegDB JSON and bigwig filenames
     with open(args.organ_mapping_json, "r") as f:
         organ_mapping = json.load(f)
-    organ = organ_mapping[args.organ]
+    organ = organ_mapping[safe_organ_name]
 
     organ_ls = []
     with Path(organ_list).open() as file:
@@ -61,8 +62,10 @@ if __name__ == '__main__':
         exit(1)
 
     tland_path = models_path / 'TLand_organSp.pickle'
+    tland_light_path = models_path / 'TLand_organSp_light.pickle'
     tland_lightest_path = models_path / 'TLand_organSp_lightest.pickle'
     model_ls = [['TLand', pickle.load(tland_path.open(mode='rb'))], 
+            ['TLand_light', pickle.load(tland_light_path.open(mode='rb'))],
             ['TLand_lightest', pickle.load(tland_lightest_path.open(mode='rb'))]]
     
 
@@ -84,12 +87,17 @@ if __name__ == '__main__':
     start = time.time()
     print(f'Predicting TLand scores for {organ}...')
     
-    df_all, pred_dict = predict(organ, generic_features, organsp_features, gt_100_tf_chip, model_ls)
-    output_df = df_all[['chrom', 'end', 'id', 'ref', 'alt']].copy()
-    if organ in gt_100_tf_chip:
-        output_df[organ+"_"+model_ls[0][0]] = pred_dict[organ][model_ls[0][0]][0]
+    if all_TLand_models:
+        selected_model_ls = model_ls
+    elif organ in gt_100_tf_chip:
+        selected_model_ls = [model_ls[0]]
     else:
-        output_df[organ+"_"+model_ls[1][0]] = pred_dict[organ][model_ls[1][0]][0]
+        selected_model_ls = [model_ls[1]]
+
+    df_all, pred_dict = predict(organ, generic_features, organsp_features, selected_model_ls)
+    output_df = df_all[['chrom', 'end', 'id', 'ref', 'alt']].copy()
+    for model_name, _ in selected_model_ls:
+        output_df[safe_organ_name+"."+model_name] = pred_dict[organ][model_name][0]
 
     output_df.rename(columns={'end': 'pos'}, inplace=True)
     output_df.to_csv(outfile, sep='\t', index=None, compression='gzip')
